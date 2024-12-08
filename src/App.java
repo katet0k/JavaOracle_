ae37079979c773ea127ea712eb946909ded9d584
Kateryna <kostucenkokata0@gmail.com>
2024-12-08 18:33:57 +0100

    0

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Виклик методів для роботи з List
+        ListOperations listOperations = new ListOperations();
+        listOperations.runListTasks();
+
+        // Виклик методів для роботи з Set
+        SetOperations setOperations = new SetOperations();
+        setOperations.runSetTasks();
+
+        // Виклик методів для роботи з Map
+        MapOperations mapOperations = new MapOperations();
+        mapOperations.runMapTasks();
+
+        // Робота з класом User
+        UserOperations userOperations = new UserOperations();
+        userOperations.runUserTasks();
     }
 }
